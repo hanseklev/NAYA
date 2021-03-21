cb35c4ab77195032d9007da023188074e7c104e6
hanseklev <59671487+hanseklev@users.noreply.github.com>
2021-03-21 20:46:53 +0100

    Delete src/components/Shop directory

--- src/components/Shop/product/stock-status.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const StockStatus = ({ stockStatus }) => {
-  if (stockStatus === "IN_STOCK")
-    return <p style={{ color: "green" }}>På lager</p>
-  else return <p style={{ color: "red" }}>Ikke på lager</p>
-}
-
-export default StockStatus

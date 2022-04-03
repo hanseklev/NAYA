5c37234b4ff667dcfb3de2f67c5ad4b857bfab7b
hanseklev2 <hanseklevstad@gmail.com>
2022-04-03 22:14:13 +0200

    removed descrption

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,7 @@ require("dotenv").config()
 module.exports = {
   siteMetadata: {
     title: `NAYA`,
-    description: `Make fair trade the only trade`,
+    description: ``,
     author: `NAYA AS`,
   },
   plugins: [
